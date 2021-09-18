6553b98a5e2bef68932e2b94433e110e5bac01df
abhishek-1210 <86312713+abhishek-1210@users.noreply.github.com>
2021-09-18 10:21:21 +0530

    Devathon update 2

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,40 +1,39 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
-
+    const history = useHistory();
+    const handleRoute = () => {
+        history.push("/");
+    }
     return (
         <>
             <nav className="navbar">
                 <div>
                     <div className="navbar-container">
-                        <Link className='navbar-logo'>
+                        <Link to='/' className='navbar-logo' onClick={handleRoute}>
                             DEVATHON
                         </Link>
                         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                             <li className='nav-item'>
-                                <Link className='nav-links' onClick={closeMobileMenu}>
-                                    Home
+                                <Link className='nav-links' onClick>
+                                    Profiles
                                 </Link>
-                                <li className='nav-item'>
-                                    <Link className='nav-links' onClick={closeMobileMenu}>
-                                        Profiles
-                                    </Link>
-                                </li>
-                                <li className='nav-item'>
-                                    <Link className='nav-links' onClick={closeMobileMenu}>
-                                        About
-                                    </Link>
-                                </li>
-                                <li className='nav-item'>
-                                    <button class="ui primary basic button">Log In</button>
-                                </li>
-                                <li className='nav-item'>
-                                    <button class="ui secondary basic button">Secondary</button>
-                                </li>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className='nav-links' onClick>
+                                    About
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <button class="ui primary basic button">Log In</button>
+                            </li>
+                            <li className='nav-item'>
+                                <button class="ui secondary basic button">Register</button>
                             </li>
                         </ul>
                     </div>
